71783460aac6f5533fbaa37845f1c5d32b3b5dd5
Mike Morgan <mike@morgan.ms>
2022-12-03 15:04:46 -0800

    Update test_common.rb

--- tools/test_common.rb
+++ tools/test_common.rb
@@ -5,6 +5,3 @@ if ENV["BUILDKITE"]
 
   Minitest::Ci.report_dir = File.join(__dir__, "../test-reports/#{ENV['BUILDKITE_JOB_ID']}")
 end
-
-require "buildkite/test_collector"
-Buildkite::TestCollector.configure(hook: :minitest)
